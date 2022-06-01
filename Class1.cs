a2717db27e24db5e2bbc1c2d3d1a28f602cd1e5b
liuxiang <liuxiang@dachkj.com>
2022-06-01 09:27:21 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateUnitTestProblem
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("some Function need Test");
+        }
+    }
+}
